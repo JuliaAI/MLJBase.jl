a8156c6a63df7fc721d22ae2ebbcea0a84dbae6e
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-02-18 19:43:26 +1300

    migrate some utilities from MLJ/src/utilities

---
+++ src/utilities.jl
@@ -0,0 +1,60 @@
+"""
+    partition(rows::AbstractVector{Int}, fractions...; shuffle=false)
+
+Splits the vector `rows` into a tuple of vectors whose lengths are
+given by the corresponding `fractions` of `length(rows)`. The last
+fraction is not provided, as it is inferred from the preceding
+ones. So, for example,
+
+    julia> partition(1:1000, 0.2, 0.7)
+    (1:200, 201:900, 901:1000)
+
+"""
+function partition(rows::AbstractVector{Int}, fractions...; shuffle::Bool=false)
+    rows = collect(rows)
+    shuffle && shuffle!(rows)
+    rowss = []
+    if sum(fractions) >= 1
+        throw(DomainError)
+    end
+    n_patterns = length(rows)
+    first = 1
+    for p in fractions
+        n = round(Int, p*n_patterns)
+        n == 0 ? (@warn "A split has only one element"; n = 1) : nothing
+        push!(rowss, rows[first:(first + n - 1)])
+        first = first + n
+    end
+    if first > n_patterns
+        @warn "Last vector in the split has only one element."
+        first = n_patterns
+    end
+    push!(rowss, rows[first:n_patterns])
+    return tuple(rowss...)
+end
+
+# the inverse of a multivalued dictionary is a mulitvalued
+# dictionary:
+function inverse(d::Dict{S,Set{T}}) where {S,T}
+    dinv = Dict{T,Set{S}}()
+    for key in keys(d)
+        for val in d[key]
+            if val in keys(dinv)
+                push!(dinv[val], key)
+            else
+                dinv[val] = Set([key,])
+            end
+        end
+    end
+    return dinv
+end
+
+function finaltypes(T::Type)
+    s = InteractiveUtils.subtypes(T)
+    if isempty(s)
+        return [T, ]
+    else
+        return reduce(vcat, [finaltypes(S) for S in s])
+    end
+end
+
