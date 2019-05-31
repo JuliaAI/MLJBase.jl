70469c8bc525c5dda735b6d6e0de6b554a98a28b
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-05-31 15:29:59 +1200

    two MLJType objects with only RNG fields different are still ==

--- src/MLJBase.jl
+++ src/MLJBase.jl
@@ -90,7 +90,8 @@ abstract type DeterministicNetwork <: Deterministic end
 
 # by default, MLJType objects are `==` if: (i) they have a common
 # supertype AND (ii) they have the same set of defined fields AND
-# (iii) their defined field values are `==`:
+# (iii) their defined field values are `==` OR the values are both
+# AbstractRNG objects.
 function ==(m1::M1, m2::M2) where {M1<:MLJType,M2<:MLJType}
     if M1 != M1
         return false
@@ -106,7 +107,9 @@ function ==(m1::M1, m2::M2) where {M1<:MLJType,M2<:MLJType}
     end
     same_values = true
     for fld in defined1
-        same_values = same_values && getfield(m1, fld) == getfield(m2, fld)
+        same_values = same_values &&
+            (getfield(m1, fld) == getfield(m2, fld) ||
+             getfield(m1, fld) isa AbsractRNG) 
     end
     return same_values
 end
