1e1910fe6bee1e02be2a5316698d3bdf384079f1
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-07-01 18:41:34 +0100

    fix test to work with julia 1.0

--- test/parameters.jl
+++ test/parameters.jl
@@ -2,23 +2,24 @@
 using Test
 using MLJBase
 
+struct Opaque
+    a::Int
+end
+
+struct Transparent
+    A::Int
+    B::Opaque
+end
+
+MLJBase.istransparent(::Transparent) = true
+
+struct Dummy <:MLJType
+    t::Transparent
+    o::Opaque
+    n::Integer
+end
+
 @testset "params method" begin
-    struct Opaque
-        a::Int
-    end
-    
-    struct Transparent
-        A::Int
-        B::Opaque
-    end
-    
-    MLJBase.istransparent(::Transparent) = true
-    
-    struct Dummy <:MLJType
-        t::Transparent
-        o::Opaque
-        n::Integer
-    end
     
     t= Transparent(6, Opaque(5))
     m = Dummy(t, Opaque(7), 42)
@@ -29,6 +30,22 @@ using MLJBase
                         n = 42)
 end
 
+mutable struct M
+    a1
+    a2
+end
+mutable struct A1
+    a11
+    a12
+end
+mutable struct A2
+    a21
+end
+mutable struct A21
+    a211
+    a212
+end
+
 @testset "getproperty, setproperty! extensions" begin
     
     m = (a1 = (a11 = 10, a12 = 20), a2 = (a21 = (a211 = 30, a212 = 40),)) 
@@ -36,22 +53,6 @@ end
     @test getproperty(m, :(a1.a12)) == 20
     @test getproperty(m, :a1) == (a11 = 10, a12 = 20)
     @test getproperty(m, :(a2.a21.a212)) == 40
-
-    mutable struct M
-        a1
-        a2
-    end
-    mutable struct A1
-        a11
-        a12
-    end
-    mutable struct A2
-        a21
-    end
-    mutable struct A21
-        a211
-        a212
-    end
 
     m = M(A1(10, 20), A2(A21(30, 40)))
     setproperty!(m, :(a2.a21.a212), 42)
