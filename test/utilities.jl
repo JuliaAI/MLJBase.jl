6bd23aac76879fa658d6ba4c17c3d0508aaaa9b5
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-08-29 12:55:57 +1200

    bump [compat] ScientificTypes = "0.1.3"
    remove duplicate code

--- test/utilities.jl
+++ test/utilities.jl
@@ -3,19 +3,7 @@ module TestUtilities
 # using Revise
 using Test
 using MLJBase
-import Random
 
-train, test = partition(1:100, 0.9)
-@test collect(train) == collect(1:90)
-@test collect(test) == collect(91:100)
-train, test = partition(1:100, 0.9, shuffle=true)
-@test length(train) == 90
-
-train, test = partition(1:100, 0.9, shuffle=true, rng=1)
-@test length(train) == 90
-
-train, test = partition(1:100, 0.9, shuffle=true, rng=Random.MersenneTwister(3))
-@test length(train) == 90
 
 end # module
 true
