de082d69bee825871845d9f51d07e4b664849212
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-10-29 15:25:02 +1300

    Resolve transpose problem #69

--- test/data.jl
+++ test/data.jl
@@ -4,7 +4,7 @@ module TestData
 using Test
 using DataFrames
 import TypedTables
-using StatsBase
+# using StatsBase
 # using JuliaDB
 using SparseArrays
 using CategoricalArrays
@@ -171,6 +171,9 @@ end
     A = hcat(v, v)
     tab = table(A)
     selectcols(tab, 1) == v
+
+    @test matrix(B) == B
+    @test matrix(B, transpose=true) == permutedims(B)
 
 end
 
