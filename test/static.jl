4bd85faa5a632d6004c923caeaf66534ae012575
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-11-15 10:03:44 +1300

    update tests

--- test/static.jl
+++ test/static.jl
@@ -8,41 +8,49 @@ mutable struct Scale <: MLJBase.Static
     scaling::Float64
 end
 
-(s::Scale)(X) = s.scaling*X
-MLJBase.inv(s::Scale, X) = X/s.scaling
+function MLJBase.transform(s::Scale, _, X) 
+    X isa AbstractVecOrMat && return X * s.scaling
+    MLJBase.table(s.scaling * MLJBase.matrix(X), prototype=X)
+end
+
+function MLJBase.inverse_transform(s::Scale, _, X)
+    X isa AbstractVecOrMat && return X / s.scaling
+    MLJBase.table(MLJBase.matrix(X) / s.scaling, prototype=X)
+end
 
 s = Scale(2)
-X = rand(2,3)
+X = randn(2, 3)
+Xt = MLJBase.table(X)
 
-# no-op:
-fitresult, _, _ = MLJBase.fit(s, 1, X)
+R = transform(s, nothing, X)
+IR = inverse_transform(s, nothing, R)
+@test IR ≈ X
 
-@test transform(s, 1, X) ≈ 2*X
-@test inverse_transform(s, 1, X) ≈ 0.5*X
+R = transform(s, nothing, Xt)
+IR = inverse_transform(s, nothing, R)
+@test MLJBase.matrix(IR) ≈ X
 
 
 ## MULTIVARIATE FUNCTION
 
 mutable struct PermuteArgs <: MLJBase.Static
-    permutation::NTuple{<:Any,Int}
+    permutation::NTuple{N,Int} where N
 end
 
-(p::PermuteArgs)(args...) =
+MLJBase.transform(p::PermuteArgs, _, args...) =
     Tuple([args[i] for i in p.permutation])
-
-p = PermuteArgs((2,3,1))
-@test p(10, 20, 30) == (20, 30, 10)
-
-MLJBase.inv(p::PermuteArgs, args...) =
+MLJBase.inverse_transform(p::PermuteArgs, _, args...) =
     Tuple([args[i] for i in sortperm(p.permutation |> collect)])
 
-@test inv(p, p(10, 20, 30)...) == (10, 20, 30)
+p = PermuteArgs((2, 3, 1))
+@test transform(p, nothing, 10, 20, 30) == (20, 30, 10)
+@test inverse_transform(p, nothing, 20, 30, 10) == (10, 20, 30)
 
 # no-op
 fitresult, _, _ = MLJBase.fit(p, 1, (1, 2, 3))
 
-@test MLJBase.transform(p, fitresult, 10, 20, 30) == (20, 30, 10) 
-@test MLJBase.inverse_transform(p, fitresult, 20, 30, 10) == (10, 20, 30)
+@test transform(p, fitresult, 10, 20, 30) == (20, 30, 10)
+@test inverse_transform(p, fitresult, 20, 30, 10) == (10, 20, 30)
 
 end
 true
