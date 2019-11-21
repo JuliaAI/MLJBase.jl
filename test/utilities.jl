d12a9c7e8646639ac9264f0fc7c6e9556d7e8981
Thibaut Lienart <ltib@me.com>
2019-11-21 18:35:09 +0100

    finaltypes tests

--- test/utilities.jl
+++ test/utilities.jl
@@ -1,10 +1,16 @@
 module TestUtilities
 
-# using Revise
 using Test
 using MLJBase
 
+@test MLJBase.finaltypes(Union{Missing,Int}) == [Union{Missing,Int64}]
+@test MLJBase.finaltypes(Float64) == [Float64]
+
+abstract type Foo end
+struct Bar <: Foo end
+struct Baz <: Foo end
+
+@test MLJBase.finaltypes(Foo) == [Bar, Baz]
 
 end # module
 true
-
