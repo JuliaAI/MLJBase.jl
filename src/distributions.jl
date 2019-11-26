e2e43f010d46a035af9c1bc4cee94b2db9bd7c77
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-11-27 09:21:43 +1300

    minor doc fix

--- src/distributions.jl
+++ src/distributions.jl
@@ -74,29 +74,25 @@ construct `d`, equal to `get.(classes(d))`.
 
 Those classes associated with non-zero probabilities.
 
-````julia
-v = categorical(["yes", "maybe", "no", "yes"])
-d = UnivariateFinite(v[1:2], [0.3, 0.7])
-pdf(d, "yes")     # 0.3
-pdf(d, v[1])      # 0.3
-pdf(d, "no")      # 0.0
-pdf(d, "house")   # throws error
-classes(d) # CategoricalArray{String,1,UInt32}["maybe", "no", "yes"]
-levels(d) # Array{String, 1}["maybe", "no", "yes"]
-support(d) # CategoricalArray{String,1,UInt32}["maybe", "no"]
-mode(d)    # CategoricalString{UInt32} "maybe"
-rand(d, 5) # CategoricalArray{String,1,UInt32}["maybe", "no", "maybe", "maybe", "no"] or similar
-d = fit(UnivariateFinite, v)
-pdf(d, "maybe") # 0.25
-```
+    v = categorical(["yes", "maybe", "no", "yes"])
+    d = UnivariateFinite(v[1:2], [0.3, 0.7])
+    pdf(d, "yes")     # 0.3
+    pdf(d, v[1])      # 0.3
+    pdf(d, "no")      # 0.0
+    pdf(d, "house")   # throws error
+    classes(d) # CategoricalArray{String,1,UInt32}["maybe", "no", "yes"]
+    levels(d) # Array{String, 1}["maybe", "no", "yes"]
+    support(d) # CategoricalArray{String,1,UInt32}["maybe", "no"]
+    mode(d)    # CategoricalString{UInt32} "maybe"
+    rand(d, 5) # CategoricalArray{String,1,UInt32}["maybe", "no", "maybe", "maybe", "no"] or similar
+    d = fit(UnivariateFinite, v)
+    pdf(d, "maybe") # 0.25
 
 One can also do weighted fits:
 
-```julia
-w = [1, 4, 5, 1] # some weights
-d = fit(UnivariateFinite, v, w)
-pdf(d, "maybe") ≈ 4/11 # true
-````
+    w = [1, 4, 5, 1] # some weights
+    d = fit(UnivariateFinite, v, w)
+    pdf(d, "maybe") ≈ 4/11 # true
 
 *Warning:* The `pdf` function will give wrong answers if the order of
  levels of any categorical element passed to the UnivariateFinite
