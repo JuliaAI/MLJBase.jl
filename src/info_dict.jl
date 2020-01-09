6d9f48e61b8134bfc6864a9d5ec624c5549d46f4
Anthony D. Blaom <anthony.blaom@gmail.com>
2020-01-09 17:51:21 +1300

    minor fix

--- src/info_dict.jl
+++ src/info_dict.jl
@@ -1,6 +1,6 @@
 # `info_dict` returns a dictionary of model trait values suitable, after
 # encoding, to serializing to TOML file. Not intended to be exposed to
-# user. 
+# user.
 
 info_dict(M::Type{<:Supervised}) = info_dict(M, SUPERVISED_TRAITS)
 info_dict(M::Type{<:Unsupervised}) = info_dict(M, UNSUPERVISED_TRAITS)
@@ -10,8 +10,8 @@ function info_dict(M::Type{<:Model}, traits)
 
     message = "$M has a bad trait declaration.\n"
 
-    ismissing(is_pure_julia(M)) || is_pure_julia(M) isa Bool ||
-        error(message*"is_pure_julia($M) must return true, false or missing. ")
+    is_pure_julia(M) == nothing || is_pure_julia(M) isa Bool ||
+        error(message*"is_pure_julia($M) must return true, false or nothing. ")
 
     :supports_weights in traits &&
         !(supports_weights(M) isa Bool) &&
@@ -19,9 +19,8 @@ function info_dict(M::Type{<:Model}, traits)
 
     :is_wrapper in traits &&
         !(is_wrapper(M) isa Bool) &&
-        error(message*"is_wrapper($M) must return true, false or missing. ")
-    
+        error(message*"is_wrapper($M) must return true, false. ")
+
     return LittleDict{Symbol,Any}(trait => eval(:($trait))(M)
                                   for trait in traits)
 end
-
