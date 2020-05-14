6ac37ce415b6c100b520bd8d49681298d9f91a67
Okon Samuel <39421418+OkonSamuel@users.noreply.github.com>
2020-05-15 00:04:57 +0100

    Update MLJBase.jl

--- src/MLJBase.jl
+++ src/MLJBase.jl
@@ -227,7 +227,6 @@ const Vec = AbstractVector
 
 const MMI = MLJModelInterface
 const FI  = MLJModelInterface.FullInterface
-const EVALUATE_THRESHOLD = 50
 # ===================================================================
 # Computational Resource
 # default_resource allows to switch the mode of parallelization
