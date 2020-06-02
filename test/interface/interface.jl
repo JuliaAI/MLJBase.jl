01393c863c4b66f1eaffff9c5c1c6b6ee8a067a1
Okon Samuel <39421418+OkonSamuel@users.noreply.github.com>
2020-06-03 00:27:09 +0100

    Update interface.jl

--- test/interface/interface.jl
+++ test/interface/interface.jl
@@ -2,7 +2,7 @@ module Interface
 
 using Test, Random, MLJBase,
       Tables, CategoricalArrays,
-      OrderedCollections, DataFrames,
+      OrderedCollections,
       TypedTables, MLJModelInterface
 
 using ..Models
