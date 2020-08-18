91796a8830fbc1d562dadc0a5c057af7eb5c5cdb
Anthony D. Blaom <anthony.blaom@gmail.com>
2020-08-18 14:34:41 +1200

    fix logic of composite updates a al #390

--- src/composition/models/methods.jl
+++ src/composition/models/methods.jl
@@ -47,37 +47,33 @@ fitted_params(::Union{Composite,Surrogate},
               fitresult::Node) =
                   fitted_params(fitresult)
 
-function update(model::Composite,
+function update(model::M,
                 verbosity::Integer,
                 fitresult::NamedTuple,
                 cache,
-                args...)
+                args...) where M <: Composite
 
-    # We must rebuild the network before fitting it whenever a field
-    # `fld` of `model` becomes *active*, meaning not *passive*. By
-    # definition, `fld` is *passive* if:
+    # This method falls back to `fit` to force rebuilding the
+    # underlying learning network if, since the last fit:
     #
-    # (1) It's `objectid` coincides with that of a model instance in
-    #     the learning network, OR
-    # (2) It's value is == to 
+    # (i) Any field associated with a model in the learning network
+    #     has been replaced with a new model instance (and not merely
+    #     mutated), OR
 
-    # If any `model` field has been replaced (and not just mutated)
-    # then we actually need to fit rather than update to force
-    # building a *new* learning network (the existing learning network
-    # still points to the old models). Similarly, if there is a field
-    # that is *not* a model that changes then, we have no choice but
-    # to rebuild the network to ensure it is incorporated (the
-    # existing learning network cannot see the change). There
-    # is no way to retain "smart fitting" in these cases.
+    # (ii) Any OTHER field has changed it's value (in the sense
+    # of `==`).
 
-    glb_node = glb(values(fitresult)...) # greatest lower bound in complete DAG
+    # Otherwise, a "smart" fit is carried out by calling `fit!` on a
+    # greatest lower bound node for nodes in the signature of the
+    # underlying learning network machine. For this it is necessary to
+    # temporarily "de-anonymize" the source nodes.
 
-    network_model_ids = objectid.(models(glb_node))
-    field_values =
-        [getproperty(model, name) for name in fieldnames(typeof(model))]
-    submodels    = filter(f->f isa Model, field_values)
-    submodel_ids = objectid.(submodels)
-    if !issubset(submodel_ids, network_model_ids)
+    network_model_fields = cache.network_model_fields
+    old_model = cache.old_model
+
+    glb_node = glb(values(fitresult)...) # greatest lower bound
+
+    if fallback(model, old_model, network_model_fields, glb_node)
         return fit(model, verbosity, args...)
     end
 
@@ -94,8 +90,29 @@ function update(model::Composite,
         rebind!(s, nothing)
     end
 
+    # record current model state:
+    cache = (sources=cache.sources,
+             data=cache.data,
+             network_model_fields=cache.network_model_fields,
+             old_model = deepcopy(model))
 
     return fitresult, cache, report(glb_node)
+
+end
+
+# helper for preceding method (where logic is explained):
+function fallback(model::M, old_model, network_model_fields, glb_node) where M
+    network_model_ids = objectid.(models(glb_node))
+    for name in fieldnames(M)
+        value = getproperty(model, name)
+        if name in network_model_fields
+            objectid(value) in network_model_ids || return true
+        else
+            old_value = getproperty(old_model, name)
+            value == old_value || return true
+        end
+    end
+    return false
 end
 
 # legacy version of above (fitresult a Node):
