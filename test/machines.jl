b260019cd5f38ed7899b28109f003e3973cbac94
Anthony D. Blaom <anthony.blaom@gmail.com>
2020-12-04 11:44:39 +1300

    add test

--- test/machines.jl
+++ test/machines.jl
@@ -4,6 +4,9 @@ using MLJBase
 using Test
 using Statistics
 using ..Models
+const MLJModelInterface = MLJBase.MLJModelInterface
+using StableRNGs
+using ..TestUtilities
 
 @load DecisionTreeRegressor
 
@@ -154,12 +157,14 @@ end
 
 end
 
-
-mutable struct Fozy <: Unsupervised end
 mutable struct Box
     matrix::Matrix{Int}
 end
 
+
+## DUMMY UNSUPERVISED MODEL
+
+mutable struct Fozy <: Unsupervised end
 MLJBase.fit(model::Fozy, verbosity, X) = minimum(X.matrix), nothing, nothing
 MLJBase.transform(model::Fozy, fitresult, new_table) =
     fill(fitresult, nrows(new_table))
@@ -167,7 +172,57 @@ MLJBase.MLJModelInterface.reformat(model::Fozy, user_data) =
     (Box(MLJBase.matrix(user_data)),)
 MLJBase.selectrows(model::Fozy, I, X...) = (Box(X[1].matrix[I,:]),)
 
+
+## BABY SUPERVISED MODEL WITH AN UPDATE METHOD
+
+mutable struct SomeModel <: Deterministic
+    n::Int
+end
+
+function MLJModelInterface.fit(model::SomeModel,
+                               verbosity,
+                               A,
+                               y)
+    n = model.n
+    cache = (A \ y)'  # all coefficients
+    n_features = length(cache)
+
+    # kill all but first n coefficients:
+    fitresult = vcat(cache[1:n], fill(0.0, n_features - n))
+
+    report = (n_features=n_features, )
+    return fitresult, cache, report
+end
+
+function MLJModelInterface.update(model::SomeModel,
+                                  verbosity,
+                                  old_fitresult,
+                                  old_cache,
+                                  A,  # ignored in this case
+                                  y)  # ignored in this case
+    n = model.n
+    cache = old_cache # coefficients already obtained in `fit`
+    n_features = length(cache)
+
+    # kill all but first n coefficients:
+    fitresult = vcat(cache[1:n], fill(0.0, n_features - n))
+
+    report = (n_features=n_features, )
+    return fitresult, cache, report
+end
+
+function MLJModelInterface.predict(::SomeModel, fitresult, Xnew)
+    Anew = MLJBase.matrix(Xnew)
+    return Anew*fitresult
+end
+
+MLJModelInterface.reformat(model::SomeModel, X, y) = (MLJBase.matrix(X), y)
+MLJModelInterface.selectrows(model::SomeModel, I, A, y) =
+    (view(A, I, :), view(y, I))
+
 @testset "overloading reformat(::Model, ...), selectrows(::Model, ...)" begin
+
+    # dummy unsupervised model:
     model = Fozy()
     args = ((x1=[10, 30, 50], x2 = [20, 40, 60]),)
     data = MLJBase.MLJModelInterface.reformat(model, args...)
@@ -177,6 +232,24 @@ MLJBase.selectrows(model::Fozy, I, X...) = (Box(X[1].matrix[I,:]),)
     mach = machine(model, args...)
     @test_logs (:info, r"Training") fit!(mach, rows=2:3);
     @test transform(mach, (x1 = 1:4, x2 = 1:4)) == [30, 30, 30, 30]
+
+    # supervised model with an update method:
+    rng = StableRNGs.StableRNG(123)
+    A = rand(rng, 8, 3)
+    y = A*[1, 2, 3]
+    X =  MLJBase.table(A)
+    model = SomeModel(1)
+    mach = machine(model, X, y)
+    @test_mach_sequence fit!(mach, rows=1:4) [(:train, mach),]
+    Xnew = selectrows(X, 1:4)
+    @test predict(mach, Xnew) ≈ A[1:4,1]
+    # mutate the model to trigger `update` call:
+    model.n=3
+    @test_mach_sequence fit!(mach, rows=1:4) [(:update, mach), ]
+    @test predict(mach, Xnew) ≈ y[1:4]
+    # change the rows to be sampled:
+    @test_mach_sequence fit!(mach) [(:train, mach),]
+    @test predict(mach, Xnew) ≈ y[1:4]
 end
 
 
