129f2e2393f002981e58b1fa8e2d70ad91f64d28
Anthony D. Blaom <anthony.blaom@gmail.com>
2020-12-14 09:30:23 +1300

    remove MACHINE_CHANNEL

--- src/init.jl
+++ src/init.jl
@@ -2,8 +2,8 @@ function __init__()
     global DEFAULT_RESOURCE = Ref{AbstractResource}(CPU1())
 
     # for testing asynchronous training of learing networks:
-    global MACHINE_CHANNEL =
-        RemoteChannel(() -> Channel(100), myid())
+    # global MACHINE_CHANNEL =
+    #     RemoteChannel(() -> Channel(100), myid())
 
     TRAIT_FUNCTION_GIVEN_NAME[:measure]      = is_measure
     TRAIT_FUNCTION_GIVEN_NAME[:measure_type] = is_measure_type
