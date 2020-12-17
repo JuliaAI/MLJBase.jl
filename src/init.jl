78408ab06d5325b3418f6759f538fe21f20f3c4a
Anthony D. Blaom <anthony.blaom@gmail.com>
2020-12-17 16:50:00 +1300

    address MACHINE_CHANNEL issue #496; update CI

--- src/init.jl
+++ src/init.jl
@@ -1,9 +1,12 @@
 function __init__()
     global DEFAULT_RESOURCE = Ref{AbstractResource}(CPU1())
 
-    # for testing asynchronous training of learing networks:
-    global MACHINE_CHANNEL =
-        RemoteChannel(() -> Channel(100), myid())
+    # for testing asynchronous training of learning networks:
+    global TESTING = parse(Bool, get(ENV, "TEST_MLJBASE", "false"))
+    if TESTING
+        global MACHINE_CHANNEL =
+            RemoteChannel(() -> Channel(100), myid())
+    end
 
     TRAIT_FUNCTION_GIVEN_NAME[:measure]      = is_measure
     TRAIT_FUNCTION_GIVEN_NAME[:measure_type] = is_measure_type
