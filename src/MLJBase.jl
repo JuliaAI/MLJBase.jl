4de8d7af23b60a079e6ef7cf6218363935938c41
Anthony D. Blaom <anthony.blaom@gmail.com>
2022-04-19 11:44:49 +1200

    small tweaks

--- src/MLJBase.jl
+++ src/MLJBase.jl
@@ -110,7 +110,7 @@ const COLUMN_WIDTH = 24
 # how deep to display fields of `MLJType` objects:
 const DEFAULT_SHOW_DEPTH = 0
 const DEFAULT_AS_CONSTRUCTED_SHOW_DEPTH = 2
-const INDENT = 4
+const INDENT = 2
 
 const Arr = AbstractArray
 const Vec = AbstractVector
--- src/resampling.jl
+++ src/resampling.jl
@@ -531,8 +531,11 @@ _short(v::Vector) = string("[", join(_short.(v), ", "), "]")
 _short(::Missing) = missing
 
 function Base.show(io::IO, ::MIME"text/plain", e::PerformanceEvaluation)
-    data = hcat(e.measure, round3.(e.measurement), e.operation,
-                [round3.(v) for v in e.per_fold])
+    _measure =  e.measure
+    _measurement = round3.(e.measurement)
+    _per_fold = [round3.(v) for v in e.per_fold]
+
+    data = hcat(_measure, _measurement, e.operation, _per_fold)
     header = ["measure", "measurement", "operation", "per_fold"]
     println(io, "PerformanceEvaluation object "*
             "with these fields:")
