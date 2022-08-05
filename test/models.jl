53b59ad1ad1bde3642b55f3c2a5e6b7a88616686
Anthony D. Blaom <anthony.blaom@gmail.com>
2022-08-05 12:10:52 +1200

    fix test

--- test/models.jl
+++ test/models.jl
@@ -1,29 +1,31 @@
+struct Potato <: Model end
+
 @testset "message_expecting_model" begin
 
     m1 = MLJBase.message_expecting_model(123)
-    @test !isempty(match(r"(Expected)", m1))
+    @test !isnothing(match(r"(Expected)", m1))
     @test isnothing(match(r"(type)", m1))
     @test isnothing(match(r"(mispelled)", m1))
 
-    m2 = MLJBase.message_expecting_model(ConstantClassifier)
-    @test !isempty(match(r"(Expected)", m2))
-    @test !isempty(match(r"(type)", m2))
+    m2 = MLJBase.message_expecting_model(Potato)
+    @test !isnothing(match(r"(Expected)", m2))
+    @test !isnothing(match(r"(type)", m2))
     @test isnothing(match(r"(mispelled)", m2))
 
     m3 = MLJBase.message_expecting_model(123, spelling=true)
-    @test !isempty(match(r"(Expected)", m3))
+    @test !isnothing(match(r"(Expected)", m3))
     @test isnothing(match(r"(type)", m3))
-    @test !isempty(match(r"(misspelled)", m3))
+    @test !isnothing(match(r"(misspelled)", m3))
 
-    m4 = MLJBase.message_expecting_model(ConstantClassifier, spelling=true)
-    @test !isempty(match(r"(Expected)", m4))
-    @test !isempty(match(r"(type)", m4))
+    m4 = MLJBase.message_expecting_model(Potato, spelling=true)
+    @test !isnothing(match(r"(Expected)", m4))
+    @test !isnothing(match(r"(type)", m4))
     @test isnothing(match(r"(misspelled)", m4))
 
 end
 
 @testset "check_ismodel" begin
-    @test isnothing(MLJBase.check_ismodel(ConstantClassifier()))
+    @test isnothing(MLJBase.check_ismodel(Potato()))
     @test_throws(
         MLJBase.err_expecting_model(123),
         MLJBase.check_ismodel(123),
@@ -33,7 +35,8 @@ end
         MLJBase.check_ismodel(123, spelling=true),
     )
     @test_throws(
-        MLJBase.err_expecting_model(ConstantClassifier),
-        MLJBase.check_ismodel(ConstantClassifier),
+        MLJBase.err_expecting_model(Potato),
+        MLJBase.check_ismodel(Potato),
     )
 end
+true
