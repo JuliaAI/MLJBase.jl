412be2127f690fb178ee10c4ba6034a6483a543b
Anthony D. Blaom <anthony.blaom@gmail.com>
2022-08-30 15:05:02 +1200

    add new NetworkComposite abstract types (shadowing deprecated Composite)

---
+++ src/composition/abstract_types.jl
@@ -0,0 +1,27 @@
+##  EXPORTED LEARNING NETWORK TYPES
+
+# For example, we want to define
+
+# abstract type ProbabilisticNetworkComposite <: Probabilistic end
+
+# but also want this for all the abstract `Model` subtypes:
+
+const NETWORK_COMPOSITE_TYPES = Symbol[]
+const network_composite_types = Any[]
+
+for T in MLJModelInterface.ABSTRACT_MODEL_SUBTYPES
+    network_composite_type_name = string(T, "NetworkComposite") |> Symbol
+    @eval(abstract type $network_composite_type_name <: $T end)
+    push!(NETWORK_COMPOSITE_TYPES, network_composite_type_name)
+    push!(network_composite_types, @eval($network_composite_type_name))
+end
+
+const NetworkComposite = Union{network_composite_types...}
+
+MLJModelInterface.is_wrapper(::Type{<:NetworkComposite}) = true
+MLJModelInterface.package_name(::Type{<:NetworkComposite}) = "MLJBase"
+
+export MLJType, Model, NetworkComposite
+for T in vcat(MMI.ABSTRACT_MODEL_SUBTYPES, NETWORK_COMPOSITE_TYPES)
+    @eval(export $T)
+end
