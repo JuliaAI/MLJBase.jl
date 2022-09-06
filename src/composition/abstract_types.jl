d9e16d5db111c341ad8cea2e5f5e7874599a550a
Anthony D. Blaom <anthony.blaom@gmail.com>
2022-09-07 09:58:59 +1200

    add new abstr types: DeterministicNetwork, ProbabilisticNetwor, etc

--- src/composition/abstract_types.jl
+++ src/composition/abstract_types.jl
@@ -1,3 +1,32 @@
+##  EXPORTED LEARNING NETWORK TYPES
+
+# For example, we want to define
+
+# abstract type ProbabilisticNetwork <: Probabilistic end
+
+# but also want this for all the abstract `Model` subtypes:
+
+const NETWORK_COMPOSITE_TYPES = Symbol[]
+const network_composite_types = Any[]
+
+for T in MLJModelInterface.ABSTRACT_MODEL_SUBTYPES
+    network_composite_type_name = string(T, "NetworkComposite") |> Symbol
+    @eval(abstract type $network_composite_type_name <: $T end)
+    push!(NETWORK_COMPOSITE_TYPES, network_composite_type_name)
+    push!(network_composite_types, @eval($network_composite_type_name))
+end
+
+const NetworkComposite = Union{network_composite_types...}
+
+MLJModelInterface.is_wrapper(::Type{<:NetworkComposite}) = true
+MLJModelInterface.package_name(::Type{<:NetworkComposite}) = "MLJBase"
+
+export MLJType, Model, NetworkComposite
+for T in vcat(MMI.ABSTRACT_MODEL_SUBTYPES, NETWORK_COMPOSITE_TYPES)
+    @eval(export $T)
+end
+
+
 ##  COMPOSITE AND SURRUGOTE MODEL TYPES
 
 # For example, we want to define
@@ -40,6 +69,6 @@ for T in surrogate_types
 end
 
 export MLJType, Model, Surrogate, Composite
-for T in vcat(MMI.ABSTRACT_MODEL_SUBTYPES, COMPOSITE_TYPES, SURROGATE_TYPES)
+for T in vcat(COMPOSITE_TYPES, SURROGATE_TYPES)
     @eval(export $T)
 end
